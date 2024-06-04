3cf5c015c066311869e399d70a5c78c7d5e5960a
MartaMykhailyna <marta.mykhailyna-pi2002@kep.nung.edu.ua>
2024-06-04 09:54:38 +0300

    did converter

--- converter_main/views.py
+++ converter_main/views.py
@@ -1,51 +1,85 @@
-from django.http import FileResponse, HttpResponseBadRequest
+from django.http import FileResponse, HttpResponseBadRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import pandas as pd
 import tempfile
 import os
 from django.shortcuts import render
 import requests
-import xlrd
-from django.http import HttpResponseBadRequest
-from pain001 import __main__
+from openpyxl import load_workbook 
+from yattag import Doc, indent 
+
 
 # Create your views here.
 def home_page(request):
     return render(request, 'converter_main/index.html')
-    
+
 @csrf_exempt
 def import_file(request):
     if request.method == 'POST':
-        # Ensure the file is included in the request
+        # Check if file is included in the request
         if 'import_file' not in request.FILES:
             return HttpResponseBadRequest("No file provided")
         
         import_file = request.FILES['import_file']
         
-        # Check if the uploaded file is an Excel file
-        if not import_file.name.endswith(('.xls', '.xlsx')):
-            return HttpResponseBadRequest("Invalid file type")
-        
-        # Read the Excel file
+        # Load Excel File 
         try:
-            df = pd.read_excel(import_file)
+            workBook=load_workbook(import_file)
         except Exception as e:
-            return HttpResponseBadRequest(f"Error reading Excel file: {e}")
+            return HttpResponseBadRequest(f"Error reading file: {str(e)}")
+
+        # Getting an object of active sheet 1 
+        workSheet = workBook.worksheets[0]
+
+        # Returning returns a triplet 
+        doc, tag, text = Doc().tagtext() 
         
-        # Create a temporary file to save the CSV
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.csv') as tmp_file:
-            csv_path = tmp_file.name
-            df.to_csv(csv_path, index=False)
+        xml_schema = 'xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.001.09" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:iso:std:iso:20022:tech:xsd:pain.001.001.09"'
+
+        with tag('Document', xml_schema):
+            with tag('CstmrCdtTrfInitn'):
+                    for row in workSheet.iter_rows(min_row=2, min_col=1): 
+                        row = [cell.value for cell in row]
+                        with tag("GrpHdr"): 
+                            with tag("CreDtTm"): 
+                                text(str(row[0])) 
+                            with tag("NbOfTxs"): 
+                                text(row[1]) 
+                            with tag("CtrlSum"): 
+                                text(row[2]) 
+                        with tag('PmtInf'):
+                            with tag('PmtInfId'):
+                                text(row[3])
+                            with tag('NbOfTxs'):
+                                text(str(row[4]))
+                            with tag('ReqdExctnDt'):
+                                with tag('Dt'):
+                                    text(row[5])
+                            with tag('DbtrAcct'):
+                                with tag('Id'):
+                                    with tag('IBAN '):
+                                        text(row[6])
+
+
+        # Generate XML string
+        result = indent( 
+            doc.getvalue(), 
+            indentation='   ', 
+            indent_text=True
+        )    
+
+        # Create a temporary file to save the XML
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.xml') as tmp_file:
+            tmp_file.write(result.encode('utf-8'))
+            tmp_file_path = tmp_file.name
         
-        # Return the CSV file as a response
-        response = FileResponse(open(csv_path, 'rb'), as_attachment=True, filename='converted_file.csv')
+        # Create a file response
+        response = HttpResponse(open(tmp_file_path, 'rb'), content_type='application/xml')
+        response['Content-Disposition'] = 'attachment; filename="pain001.xml"'
         
-        # Clean up the temporary file after the response is complete
-        response['X-Accel-Buffering'] = 'no'
-        response['X-Accel-Expires'] = '0'
-        response['Content-Disposition'] = f'attachment; filename="converted_file.csv"'
-        response['Content-Length'] = os.path.getsize(csv_path)
+        # Clean up the temporary file
+        os.remove(tmp_file_path)
         
         return response
-
-    return HttpResponseBadRequest("Invalid request method")
+    else:
+        return HttpResponseBadRequest("Only POST method is allowed")
